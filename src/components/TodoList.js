e918e50cc6fc0dca147c7c33f3882257e407bcbe
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-30 10:42:16 +0800

    refactor: remove div in TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,11 +7,11 @@ const TodoList = () => {
     const todoItems = useSelector((state) => state.todoList.todoList);
 
     return (
-        <div className="App">
+        <>
             <h1>Todo List</h1>
             <TodoGroup todoItems={todoItems} />
             <TodoGenerator />
-        </div>
+        </>
     );
 };
 
