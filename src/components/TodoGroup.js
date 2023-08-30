fce98eca3af1f4497d4e30b883e43489ec922fc3
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-30 10:34:54 +0800

    refactor: remove index in TodoGroup

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -2,8 +2,8 @@ import TodoItem from "./TodoItem";
 
 const TodoGroup = (props) => {
     const todoItems = props.todoItems;
-    return todoItems.map((todoItem, index) => {
-        return <TodoItem todoItem={todoItem} key={todoItem.id} index={index} />;
+    return todoItems.map((todoItem) => {
+        return <TodoItem todoItem={todoItem} key={todoItem.id} />;
     });
 };
 
