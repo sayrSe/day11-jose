a65a6860196eacabf012675c097d67af3da4c9fa
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-31 02:27:41 +0800

    refactor: remove unused actions in slice

--- src/components/todoListSlice.js
+++ src/components/todoListSlice.js
@@ -9,28 +9,8 @@ const todoListSlice = createSlice({
         resetAllTodoItems: (state, action) => {
             state.todoList = action.payload;
         },
-        addTodoItem: (state, action) => {
-            state.todoList.push(action.payload);
-        },
-        toggleTodoItem: (state, action) => {
-            const todoItem = state.todoList.find(
-                (todoItem) => todoItem.id === action.payload
-            );
-            todoItem.done = !todoItem.done;
-        },
-        deleteTodoItem: (state, action) => {
-            const newTodoList = state.todoList.filter(
-                (todoItem) => todoItem.id !== action.payload
-            );
-            state.todoList = newTodoList;
-        },
     },
 });
 
-export const {
-    resetAllTodoItems,
-    addTodoItem,
-    toggleTodoItem,
-    deleteTodoItem,
-} = todoListSlice.actions;
+export const { resetAllTodoItems } = todoListSlice.actions;
 export default todoListSlice.reducer;
