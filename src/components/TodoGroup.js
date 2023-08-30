b666f92323598b8ce67cb02bc866c79800160673
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-30 16:20:09 +0800

    feat: apply axiot and mockApi when updating todoItem

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -1,7 +1,8 @@
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 const TodoGroup = (props) => {
-    const todoItems = props.todoItems;
+    const todoItems = useSelector((state) => state.todoList.todoList);
     return todoItems.map((todoItem) => {
         return (
             <TodoItem
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import * as todoApi from "../api/todoApi";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
 import "./todoList.css";
 import { resetAllTodoItems } from "./todoListSlice";
 
-const TodoList = (props) => {
+const TodoList = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         async function fetchData() {
@@ -15,11 +15,10 @@ const TodoList = (props) => {
         }
         fetchData();
     }, []);
-    const todoItems = useSelector((state) => state.todoList.todoList);
     return (
         <>
             <h1>Todo List</h1>
-            <TodoGroup todoItems={todoItems} isDone={props.isDone} />
+            <TodoGroup />
             <TodoGenerator />
         </>
     );
