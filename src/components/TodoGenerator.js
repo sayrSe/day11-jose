48bb581dada106f6a4a5cc28a1a2dcfb6a167708
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-30 23:22:06 +0800

    feat: apply ant design in todo generator

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -1,3 +1,4 @@
+import { Button, Input, Row, Space } from "antd";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import * as todoApi from "../../src/api/todoApi";
@@ -33,19 +34,25 @@ const TodoGenerator = () => {
     };
 
     return (
-        <div>
-            <input
-                type="text"
-                value={itemInput}
-                onChange={onItemChange}
-                className="item-input"
-                placeholder="What are you going to do today?"
-                id="itemInput"
-            />
-            <button onClick={addItem} className="add-button">
-                Add
-            </button>
-        </div>
+        <Row>
+            <Space.Compact
+                style={{
+                    width: "100%",
+                    marginTop: "10px",
+                }}
+            >
+                <Input
+                    size="large"
+                    value={itemInput}
+                    placeholder="What are you going to do today?"
+                    onChange={onItemChange}
+                    id="itemInput"
+                />
+                <Button size="large" type="primary" onClick={addItem}>
+                    Add
+                </Button>
+            </Space.Compact>
+        </Row>
     );
 };
 
